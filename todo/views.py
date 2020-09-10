86fdf0bb652181057abc7001b2ad25b5e3df257d
abhigyan <abhigyan.gupta@verts.co.in>
2020-09-11 04:24:02 +0530

    [ADD] First commit

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
 import random
 # Create your views here.
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 def signupuser(request):
     if request.method=='GET':
@@ -18,6 +21,28 @@ def signupuser(request):
         else:
             return render(request, 'todo/signupuser.html',{'error':"Password Does't Match."} )
 
+def loginuser(request):
+    print("55555555555555555")
+    if request.method=="GET":
+        return render(request, 'todo/loginuser.html')
+    else:
+        print("2222")
+        user=authenticate(request,username=request.POST['username'],pwd=request.POST['password'])
+        print("ggg",user)
+        if user is None:
+            return render(request, 'todo/loginuser.html',{'error':"User name Or Password Does't Match" })
+        else:
+            login(request, user)
+            return redirect('loginuser')
+
+
+
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html',{'current':"Welcome To Your Current TO Do's"})
+
+def logoutuser(request):
+    if request.method=='POST':
+        logout(request)
+        return redirect('home')
+
 
--- tododesk/urls.py
+++ tododesk/urls.py
@@ -19,6 +19,9 @@ from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
     path('signupuser/', views.signupuser,name='signupuser'),
+    path('logoutuser/', views.logoutuser,name='logoutuser'),
+    path('loginuser/', views.loginuser,name='loginuser'),
     path('current/', views.currenttodos,name='currenttodos'),
 ]
